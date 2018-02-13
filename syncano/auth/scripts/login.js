32f1ab62f90572768dfb6458bd374471653ec714
Kasper Mikiewicz <IderedPL@gmail.com>
2018-02-13 12:27:48 +0100

    Update login.js

--- syncano/auth/scripts/login.js
+++ syncano/auth/scripts/login.js
@@ -1,34 +1,36 @@
 import fetch from 'axios'
 import {users, data, response} from 'syncano-server'
 
-const { username, password } = ARGS.POST
-const AUTH_URL = `https://api.syncano.io/v2/instances/${META.instance}/users/auth/`
+export default (ctx) => {
+  const { username, password } = args.ctx
+  const AUTH_URL = `https://api.syncano.io/v2/instances/${META.instance}/users/auth/`
 
-users
-  .where('username', 'eq', username)
-  .firstOrFail()
-  .then(authorizeUser)
-  .catch(respondWithInvalidCredentials)
-
-function authorizeUser() {
-  fetch({
-    url: AUTH_URL,
-    method: 'POST',
-    data: JSON.stringify({
-      username,
-      password
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-      'X-API-KEY': META.token
-    }
-  })
-    .then(({data}) => {
-      response.json({token: data.user_key, username: data.username})
-    })
+  users
+    .where('username', 'eq', username)
+    .firstOrFail()
+    .then(authorizeUser)
     .catch(respondWithInvalidCredentials)
-}
 
-function respondWithInvalidCredentials() {
-  response.json({data: {detail: 'Given credentials are invalid.'}}, 400)
+  function authorizeUser() {
+    fetch({
+      url: AUTH_URL,
+      method: 'POST',
+      data: JSON.stringify({
+        username,
+        password
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-KEY': META.token
+      }
+    })
+      .then(({data}) => {
+        response.json({token: data.user_key, username: data.username})
+      })
+      .catch(respondWithInvalidCredentials)
+  }
+
+  function respondWithInvalidCredentials() {
+    response.json({data: {detail: 'Given credentials are invalid.'}}, 400)
+  }
 }
